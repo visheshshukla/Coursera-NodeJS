82e6e441628bffa215e240e38d5f0630af0ae5d5
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-09-20 02:24:25 +0530

    mongoose part 1

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-const dboper = require('./operations');
+const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/';
-const dbname = 'conFusion';
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
 
-MongoClient.connect(url).then((client) => {
+connect.then((db) => {
 
     console.log('Connected correctly to server');
-    const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes")
-        .then((result) => {
-            console.log("Insert Document:\n", result.ops);
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
 
-            return dboper.findDocuments(db, "dishes");
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
         })
-        .then((docs) => {
-            console.log("Found Documents:\n", docs);
+        .then((dishes) => {
+            console.log(dishes);
 
-            return dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes");
-
+            return Dishes.remove({});
         })
-        .then((result) => {
-            console.log("Updated Document:\n", result.result);
-
-            return dboper.findDocuments(db, "dishes");
+        .then(() => {
+            return mongoose.connection.close();
         })
-        .then((docs) => {
-            console.log("Found Updated Documents:\n", docs);
-                            
-            return db.dropCollection("dishes");
-        })
-        .then((result) => {
-            console.log("Dropped Collection: ", result);
+        .catch((err) => {
+            console.log(err);
+        });
 
-            return client.close();
-        })
-        .catch((err) => console.log(err));
-
-})
-.catch((err) => console.log(err));
+});
